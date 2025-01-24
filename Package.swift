e2756722c873510d3e8dff82f1480f47ab365d6f
Tikiram Ruiz <tikiram@icloud.com>
2025-01-23 19:06:32 -0600

    refactor: configure app

--- Package.swift
+++ Package.swift
@@ -9,19 +9,16 @@ let package = Package(
   dependencies: [
     // 💧 A server-side Swift web framework.
     .package(url: "https://github.com/vapor/vapor.git", from: "4.105.0"),
-    // JWT
-    .package(url: "https://github.com/vapor/jwt.git", from: "4.2.1"),
     // AWS
     .package(url: "https://github.com/awslabs/aws-sdk-swift", from: "1.0.69"),
     // Shared
-    .package(url: "https://github.com/tikiram/vapor-utils.git", from: "0.3.0"),
+    .package(url: "https://github.com/tikiram/vapor-utils.git", from: "0.7.0"),
   ],
   targets: [
     .executableTarget(
       name: "App",
       dependencies: [
         .product(name: "Vapor", package: "vapor"),
-        .product(name: "JWT", package: "jwt"),
         .product(name: "AWSDynamoDB", package: "aws-sdk-swift"),
         .product(name: "SharedBackend", package: "vapor-utils"),
         "Sendgrid",
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,8 +1,6 @@
 import SharedBackend
 import Vapor
-import JWTKit
 
-let PRIVATE_KEY_STRING = Environment.get("PRIVATE_KEY_STRING")
 let SENDGRID_API_KEY = Environment.get("SENDGRID_API_KEY")
 
 enum EnvironmentValueError: Error {
@@ -10,9 +8,8 @@ enum EnvironmentValueError: Error {
 }
 
 public func configure(_ app: Application) async throws {
-  guard let PRIVATE_KEY_STRING else {
-    throw EnvironmentValueError.undefined("PRIVATE_KEY")
-  }
+
+  let appUtils = AppUtils(app)
 
   guard let SENDGRID_API_KEY else {
     throw RuntimeError("SENDGRID_API_KEY not defined")
@@ -20,42 +17,17 @@ public func configure(_ app: Application) async throws {
 
   app.http.server.configuration.port = 3000
 
-  try configureCors(app)
+  try appUtils.configureCors()
 
   app.middleware.use(RepoErrorMiddleware())
 
   // email configuration
   app.sendGridConfiguration = .init(apiKey: SENDGRID_API_KEY)
 
-  setVaporWithCompanyStandardJSONEncoderDecoder()
-  
-  let pkey=PRIVATE_KEY_STRING.replacingOccurrences(of: "\\n", with: "\n")
-  
-  let privateKey = try ECDSAKey.private(pem: pkey)
+  appUtils.setCompanyStandardJSONEncoderDecoder()
 
-  app.jwt.signers.use(.es256(key: privateKey), kid: "private", isDefault: true)
+  try appUtils.configurePrivateKey()
 
   // register routes
   try routes(app)
-}
-
-func configureCors(_ app: Application) throws {
-  
-  guard let corsOriginsString = Environment.get("CORS_ORIGINS") else {
-    throw RuntimeError("CORS_ORIGINS not defined")
-  }
-  let origins = corsOriginsString.split(separator: ",").map({ $0.trim() })
-  
-  let corsConfiguration = CORSMiddleware.Configuration(
-    allowedOrigin: .any(origins),
-    allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
-    allowedHeaders: [
-      .accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent,
-      .accessControlAllowOrigin, .setCookie, .setCookie2,
-    ],
-    allowCredentials: true
-  )
-  let cors = CORSMiddleware(configuration: corsConfiguration)
-  // cors middleware should come before default error middleware using `at: .beginning`
-  app.middleware.use(cors, at: .beginning)
 }
