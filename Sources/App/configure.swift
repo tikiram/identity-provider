535bf64cd130f6dd02f80ab40c7663eddd1641f1
Tikiram Ruiz <tikiram@icloud.com>
2025-02-05 22:15:45 -0600

    update

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,33 +1,30 @@
 import SharedBackend
 import Vapor
 
-let SENDGRID_API_KEY = Environment.get("SENDGRID_API_KEY")
-
-enum EnvironmentValueError: Error {
-  case undefined(String)
-}
-
 public func configure(_ app: Application) async throws {
+
+  app.http.server.configuration.port = 3000
+  app.middleware.use(RepoErrorMiddleware())
+  
+  try configureEmail(app)
 
   let appUtils = AppUtils(app)
 
-  guard let SENDGRID_API_KEY else {
-    throw RuntimeError("SENDGRID_API_KEY not defined")
-  }
-
-  app.http.server.configuration.port = 3000
-
   try appUtils.configureCors()
-
-  app.middleware.use(RepoErrorMiddleware())
-
-  // email configuration
-  app.sendGridConfiguration = .init(apiKey: SENDGRID_API_KEY)
-
   appUtils.setCompanyStandardJSONEncoderDecoder()
-
   try appUtils.configurePrivateKey()
 
   // register routes
   try routes(app)
+}
+
+func configureEmail(_ app: Application) throws {
+  // TODO: migrate to AWS Simple Email Service
+
+  guard let SENDGRID_API_KEY = Environment.get("SENDGRID_API_KEY") else {
+    throw RuntimeError("SENDGRID_API_KEY not defined")
+  }
+
+  // email configuration
+  app.sendGridConfiguration = .init(apiKey: SENDGRID_API_KEY)
 }
